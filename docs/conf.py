c22c713982dbbb4da29d682994c62ca3b8d4053e
Barnaby Shearer <b@zi.is>
2021-11-11 16:03:54 +0000

    Add docs/conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Sphinx config."""
+
+extensions = ["sphinxcontrib.autoprogram", "sphinx.ext.autodoc"]
+
+project = "`ctypeslib` converted libnfc and libfreefare."
+copyright = "2021, Barnaby Shearer"
+author = "Barnaby Shearer"
+
+master_doc = "index"
